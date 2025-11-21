b0f123d47afcaae1c23e0f782f031e0f63b02721
sanjays-ms <v-sanjayse@microsoft.com>
2025-11-21 17:24:12 +0530

    Added compatible NuGet packages to test NuGet restore functionality

--- DemoAPI/Controllers/WeatherForecastController.cs
+++ DemoAPI/Controllers/WeatherForecastController.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace DemoAPI.Controllers
 {
@@ -6,10 +9,17 @@ namespace DemoAPI.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
+        private readonly ILogger<WeatherForecastController> _logger;
+
         private static readonly string[] Summaries =
         [
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         ];
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
 
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
@@ -21,6 +31,37 @@ namespace DemoAPI.Controllers
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
+        }
+
+        [HttpGet("json-test")]
+        public IActionResult TestNewtonsoftJson()
+        {
+            // Demonstrate Microsoft.Extensions.Logging functionality
+            _logger.LogInformation("Testing Newtonsoft.Json and Microsoft.Extensions.Logging packages");
+
+            // Demonstrate Newtonsoft.Json functionality
+            var testData = new
+            {
+                Message = "Newtonsoft.Json is working!",
+                Timestamp = DateTime.UtcNow,
+                TargetFramework = System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription,
+                Packages = new
+                {
+                    NewtonsoftJson = "13.0.3",
+                    MicrosoftExtensionsLogging = "10.0.0"
+                }
+            };
+
+            // Serialize to JSON string
+            string jsonString = JsonConvert.SerializeObject(testData, Formatting.Indented);
+
+            // Parse back to JObject to demonstrate JSON manipulation
+            JObject parsedJson = JObject.Parse(jsonString);
+            parsedJson["AdditionalInfo"] = "This endpoint tests NuGet package restore";
+
+            _logger.LogInformation("Successfully tested both NuGet packages");
+
+            return Ok(parsedJson);
         }
     }
 }
--- DemoAPI/Program.cs
+++ DemoAPI/Program.cs
@@ -1,4 +1,3 @@
-
 namespace DemoAPI
 {
     public class Program
@@ -8,23 +7,14 @@ namespace DemoAPI
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            //builder.Services.AddOpenApi();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            //if (app.Environment.IsDevelopment())
-            //{
-            //    app.MapOpenApi();
-            //}
-
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
 
             app.MapControllers();
 
